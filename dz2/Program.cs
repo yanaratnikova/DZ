349d5216eb7a8c4597f0278e8197ef97bf1f1f0f
Yana_Ratnikova <yardley1@yandex.ru>
2023-02-25 22:39:09 +0300

    Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел

---
+++ dz2/Program.cs
@@ -0,0 +1,39 @@
+﻿// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел
+using System;
+
+public class MyProgram
+{
+    public static void Main(string[] args)
+    {
+        int a, b, c, max;
+        Console.WriteLine("Введите три целых числа: ");
+        
+        a = (int) inputValue();
+        b = (int) inputValue();
+        c = (int) inputValue();
+        if (a > b)
+        {
+            max = a;
+        }
+        else
+        {
+            max = b;
+        }
+        if (c > max)
+        {
+            max = c;
+        }
+        Console.Write("Максимальное число: ");
+        Console.WriteLine(max);
+    }
+    
+    // .NET can only read single characters or entire lines from the
+    // console. The following function safely reads a double value.
+    private static double inputValue()
+    {
+        double result;
+        while (!double.TryParse(Console.ReadLine(), out result));
+        return result;
+    }
+}
+
