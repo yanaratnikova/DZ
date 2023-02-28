c2263dfc1df3bda4cf84825ab5b9a33144e502f8
Yana_Ratnikova <yardley1@yandex.ru>
2023-02-28 10:48:42 +0300

    Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.

---
+++ dz6/Program.cs
@@ -0,0 +1,43 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает,
+// что третьей цифры нет
+using System;
+
+public class MyProgram
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Введите число: ");
+        int n, res;
+        
+        n = (int) inputValue();
+        if ((double) n / 100 < 1)
+        {
+            Console.WriteLine("Третьей цифры нет.");
+        }
+        else
+        {
+            if ((double) (n - n % 10) / 10 < 100)
+            {
+            }
+            else
+            {
+                while ((double) (n - n % 10) / 10 > 100)
+                {
+                    n = (int) ((double) (n - n % 10) / 10);
+                }
+            }
+            res = n % 10;
+            Console.WriteLine(res);
+        }
+    }
+    
+    // .NET can only read single characters or entire lines from the
+    // console. The following function safely reads a double value.
+    private static double inputValue()
+    {
+        double result;
+        while (!double.TryParse(Console.ReadLine(), out result));
+        return result;
+    }
+}
+
