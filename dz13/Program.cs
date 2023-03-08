6f8395d78b197aa0b2e9d0b643eb471b71d03659
Yana_Ratnikova <yardley1@yandex.ru>
2023-03-08 20:08:00 +0300

    Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран

---
+++ dz13/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран
+
+int Prompt()
+{
+    Console.WriteLine("Введите количество елементов");
+    return Convert.ToInt32(Console.ReadLine());
+}
+int[] CreateArray(int size)
+{
+    int[] array = new int [size];
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = new Random().Next(0,10);
+    }
+    return array;
+}
+int[] newArray = CreateArray(Prompt());
+
+Console.WriteLine($"[{String.Join("," , newArray)}]");
+
