ad9abadf1b5eca65dcbb23026a0b7a5c883161e1
Yana_Ratnikova <yardley1@yandex.ru>
2023-03-08 19:52:16 +0300

    Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.

---
+++ dz12/Program.cs
@@ -0,0 +1,38 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+using System;
+
+public class MyProgram
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Введите число: ");
+        int sum, n;
+        
+        n = (int) inputValue();
+        if (n < 0 && n == 0)
+        {
+            Console.WriteLine("Это не целое число. Повторите ввод.");
+        }
+        else
+        {
+            sum = 0;
+            while ((double) n / 10 > 0)
+            {
+                sum = sum + n % 10;
+                n = (int) ((double) (n - n % 10) / 10);
+            }
+            sum = sum + n % 10;
+            Console.WriteLine(sum);
+        }
+    }
+    
+    // .NET can only read single characters or entire lines from the
+    // console. The following function safely reads a double value.
+    private static double inputValue()
+    {
+        double result;
+        while (!double.TryParse(Console.ReadLine(), out result));
+        return result;
+    }
+}
+
